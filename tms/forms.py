5c5ae1f2faf9edbb0bf37ec021a94f5bfafcaa50
Anuvab Chakravarty <anucbs2018@gmail.com>
2023-11-05 13:25:50 +0530

    TMS Beta Version (Runs on venv)

---
+++ tms/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Length,EqualTo,Email,DataRequired,ValidationError
+from tms.models import User
+class RegisterForm(FlaskForm):
+
+    def validate_username(self,username_to_check):
+        user=User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exists!! Try a different Username')
+        
+    def validate_email_address(self,email_address_to_check):
+        email_address=User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address already exists!! Try a different e-mail address')
+    username=StringField(label="Enter Username:",validators=[Length(min=2),DataRequired()])
+    email_address=StringField(label="Enter E-mail Address:",validators=[Email(),DataRequired()])
+    password1=PasswordField(label="Enter Password:",validators=[Length(min=6),DataRequired()])
+    password2=PasswordField(label="Confirm Password:",validators=[EqualTo('password1'),DataRequired()])
+    submit=SubmitField(label="Create Account")
+
+class LoginForm(FlaskForm):
+    username = StringField(label='Enter Username:', validators=[DataRequired()])
+    password = PasswordField(label='Enter Password:', validators=[DataRequired()])
+    submit = SubmitField(label='Sign in')
+
+class SetBidForm(FlaskForm):
+    submit = SubmitField(label='Set Bid for Tender')
+
+class SellTenderForm(FlaskForm):
+    submit = SubmitField(label='Sell Tender')
---
+++ tms/routes.py
@@ -0,0 +1,80 @@
+from tms import app
+from flask import render_template,redirect,url_for,flash,request
+from tms.models import Item,User
+from tms import db
+from tms.models import Item
+from tms.forms import RegisterForm,LoginForm,SellTenderForm,SetBidForm
+from flask_login import login_user,logout_user,login_required,current_user
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been successfully logged out of your ProTender Account.",category='info')
+    return redirect(url_for("home_page"))
+
+@app.route('/market/',methods=['GET','POST'])
+@login_required
+def market_page():
+    bid_form=SetBidForm()
+    sell_form=SellTenderForm()
+    if request.method=='POST':
+        # Set Bid Logic
+        bidding=request.form.get('bidding')
+        bid_obj=Item.query.filter_by(name=bidding).first()
+        if bid_obj:
+            if(current_user.can_set_bid(bid_obj)):
+                bid_obj.bid(current_user)
+                flash(f"Congratulations!! You have successfully set your bid for the {bid_obj.name} tender at ₹{bid_obj.price}", category='success')
+            else:
+                flash("Unfortunately the bid cannot be made due to Insufficient Balance", category='danger')
+        # Selling Tender Logic
+        selling=request.form.get('selling')
+        sell_obj=Item.query.filter_by(name=selling).first()
+        if sell_obj:
+            if(current_user.can_sell(sell_obj)):
+                sell_obj.sell(current_user)
+                flash(f"Congratulations!! You have successfully let go of your bid for the {sell_obj.name} tender", category='success')
+            else:
+                flash("Unfortunately you cannot let go of this bid because you are not holding it", category='danger')
+        return redirect(url_for('market_page'))
+        
+    if request.method=='GET':        
+        items = Item.query.filter_by(owner=None)
+        owned_items=Item.query.filter_by(owner=current_user.id)
+        return render_template('market.html', items=items, bid_form=bid_form, owned_items=owned_items, sell_form=sell_form)
+
+@app.route('/register',methods=['GET','POST'])
+def register_page():
+    form=RegisterForm()
+    if form.validate_on_submit():
+        user_to_create=User(username=form.username.data,email_address=form.email_address.data,password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        login_user(user_to_create)
+        flash(f'Account created successfully. You are now logged in as {user_to_create.username}',category='success')
+        return redirect(url_for('market_page'))
+    if form.errors != {} : # no erros from the validation
+        for err_msg in form.errors.values():
+            flash(f'User Validation Error: {err_msg}',category='danger')
+    return render_template('register.html',form=form)
+
+@app.route('/login',methods=['GET','POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and Password do not Match! Please try again', category='danger')
+
+    return render_template('login.html', form=form)
