42646ffc4088eafded3a15b1e3654d1a02956733
Anuvab Chakravarty <anucbs2018@gmail.com>
2023-11-13 01:42:58 +0530

    Added Basic Admin Interface

--- tms/__init__.py
+++ tms/__init__.py
@@ -3,7 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_marshmallow import Marshmallow
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+# from tms.models import User,Item
+admin=Admin()
 app = Flask(__name__)
+admin.init_app(app)
 ma = Marshmallow(app) 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///tms.db'
 app.config['SECRET_KEY']='E39058C761AB'
@@ -13,3 +18,13 @@ login_manager=LoginManager(app)
 login_manager.login_view="login_page"
 login_manager.login_message_category="info"
 from tms import routes
+
+class UserView(ModelView):
+    can_view_details=True
+
+class ItemView(ModelView):
+    can_view_details=True
+
+from tms.models import User,Item # Using Lazy importing to avoid circular imports
+admin.add_view(UserView(User,db.session))
+admin.add_view(ItemView(Item,db.session))
